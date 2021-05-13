f00b876f24ea153b7877287f81a31de4fc5d3a51
Shubham Sagar <shubham.sagar1395@gmail.com>
2021-05-13 15:51:11 +0530

    Updated exe to try on Mac

--- dev/setup.py
+++ dev/setup.py
@@ -1,10 +1,10 @@
 from cx_Freeze import setup, Executable
 
 includefiles = ['images']
-base = "Win32GUI"
+# base = "Win32GUI"
 
 setup(name="CowinSlotTracker",
       version="1.0",
       description="An application to track vacant vaccination slots.",
-      executables=[Executable("CowinSlotTracker.py", base=base)],
+      executables=[Executable("CowinSlotTracker.py")],
       options={'build_exe': {'include_files':includefiles}})
